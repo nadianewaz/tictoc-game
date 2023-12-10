911e26f48c2a1cb1a031fef6a4dab8d5ed03d964
nadianewaz <aninditaahmed@proton.me>
2023-12-10 15:59:47 +0600

    state update

--- src/App.js
+++ src/App.js
@@ -27,11 +27,10 @@ export default function App( xIsNext, squares, onPlay ) {
       nextSquares[i] = "C";
     }
       
-    
     setSquares(nextSquares);
     setXIsNext(!xIsNext); 
   }
-
+   
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -39,7 +38,6 @@ export default function App( xIsNext, squares, onPlay ) {
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
-
 
   return (
     <>
@@ -66,31 +64,6 @@ export default function App( xIsNext, squares, onPlay ) {
     </>
   );
 }
-
-  
-  
-export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
-
-  function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
-  }
-
-  return (
-    <div className="game">
-      <div className="game-board">
-       <App xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}  />   
-      </div>
-      <div className="game-info">
-        <ol> {} </ol>
-      </div>
-    </div>
-  );
-}
-  
 
 function calculateWinner(squares) {
   const lines = [
