1e9bc346f324b046461f4c650aef255467b31930
nadianewaz <aninditaahmed@proton.me>
2023-12-10 14:39:16 +0600

    component add

--- src/App.js
+++ src/App.js
@@ -11,11 +11,9 @@ function Box( {value, onValueClick} ) {
   </button>;
 }
 
-export default function App() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState( Array(9).fill(null) );
+export default function App( xIsNext, squares, onPlay ) {
   
-  function handleClick (i) {   
+    function handleClick (i) {   
     if (squares[i] || calculateWinner(squares) ) {
       return;
     }
@@ -68,6 +66,31 @@ export default function App() {
     </>
   );
 }
+
+  
+  
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+       <App xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}  />   
+      </div>
+      <div className="game-info">
+        <ol> {} </ol>
+      </div>
+    </div>
+  );
+}
+  
 
 function calculateWinner(squares) {
   const lines = [
